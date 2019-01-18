7f19d2a0e4c829d82271f3dec28902b6a1b93ef9
David Tucker <david@davidtucker.net>
2019-01-18 10:44:13 -0600

    Updating form

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -7,7 +7,12 @@ import styles from './footer.module.css'
 export default ({ }) => (
     <form name="contact" method="POST" action="/success/" data-netlify="true" data-netlify-honeypot="bot-field">
         <input type="hidden" name="form-name" value="contact" />
-        <input type="hidden" name="bot-field" />
+        <p hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </p>
         <div className="field half first">
             <label htmlFor="name">Name</label>
             <input type="text" name="name" id="name" />
