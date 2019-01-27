23ee2acc1fe59a2e723434be593c3714294d1339
David Tucker <david@davidtucker.net>
2019-01-26 22:30:51 -0500

    Updating ArticleCard structure

--- src/components/article-card.js
+++ src/components/article-card.js
@@ -5,34 +5,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import HamburgerMenu from 'react-hamburger-menu'
 import { CSSTransitionGroup } from 'react-transition-group'
+import PropTypes from 'prop-types'
 
-class ArticleCard extends React.Component { 
-
-  constructor (props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-    return (
-      <div className={styles.articleCard}>
-        <div className={styles.imageSide} style={{
-            backgroundImage: `url('${this.props.articleData.heroImage.fluid.src}')`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center top'
-        }}>
-        </div>
-        <div className={styles.textSide}>
-            <h4>Article</h4>
-            <h2><Link to={`/blog/${this.props.articleData.slug}`}>{this.props.articleData.title}</Link></h2>
-            <p className={styles.description} dangerouslySetInnerHTML={{
-                __html: this.props.articleData.description.childMarkdownRemark.html
-              }}></p>
-        </div>
+const ArticleCard = ({ articleData }) => (
+  <div className={styles.articleCard}>
+      <div className={styles.imageSide} style={{
+          backgroundImage: `url('${articleData.heroImage.fluid.src}')`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center top'
+      }}>
       </div>
-    )
-  }
+      <div className={styles.textSide}>
+          <h4>Article</h4>
+          <h2><Link to={`/blog/${articleData.slug}`}>{articleData.title}</Link></h2>
+          <p className={styles.description} dangerouslySetInnerHTML={{
+              __html: articleData.description.childMarkdownRemark.html
+            }}></p>
+      </div>
+    </div>
+)
+
+ArticleCard.propTypes = {
+  articleData: PropTypes.object,
 }
 
 export default ArticleCard
