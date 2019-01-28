e508e83bb6c57c6a1d7d09dc45088fec5e436823
David Tucker <david@davidtucker.net>
2019-01-28 10:26:05 -0500

    Fixing blog page query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -54,6 +54,7 @@ export const pageQuery = graphql`
             }
           }
           description {
+            description
             childMarkdownRemark {
               html
             }
